1e208a3ddbc8c4c439214a2362b94f73d9ead76f
ligotommy <116342243+ligotommy@users.noreply.github.com>
2022-12-21 15:32:48 +0200

    added new game function and leader highlighter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const homeNumEl = document.getElementById("homeNum")
+const homeGuestEl = document.getElementsByClassName("upper-part")
 const guestNumEl = document.getElementById("guestNum")
-let homeWidth = 80
-let guestWidth = 80
+let homeWidth = 80, guestWidth = 80
 
 
 function add(num, whichOne) {
@@ -20,4 +20,26 @@ function add(num, whichOne) {
             guestNumEl.style.width = `${guestWidth}px`
         }
     }
+    let homePoints = parseInt(homeNumEl.textContent)
+    let guestPoints = parseInt(guestNumEl.textContent)
+    if (homePoints > guestPoints) {
+        homeGuestEl[0].style.color = "darkgreen"
+        homeGuestEl[1].style.color = "darkred"
+    } else if (homePoints < guestPoints) {
+        homeGuestEl[0].style.color = "darkred"
+        homeGuestEl[1].style.color = "darkgreen"
+    } else {
+        homeGuestEl[0].style.color = "white"
+        homeGuestEl[1].style.color = "white"
+    }
+}
+
+
+function newGame() {
+    homeGuestEl[0].style.color = "white"
+    homeGuestEl[1].style.color = "white"
+    homeNumEl.textContent = 0, guestNumEl.textContent = 0
+    homeWidth = 80, guestWidth = 80
+    guestNumEl.style.width = `${guestWidth}px`
+    homeNumEl.style.width = `${homeWidth}px`
 }
